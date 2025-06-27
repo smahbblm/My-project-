76b8c268d5713c09302fb2c8d11e8191fcc965c8
smahan blm <boulmaneismahane@gmail.com>
2025-06-27 09:51:40 +0100

    c'est un message

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 
     public static void main(String[] args){
         System.out.println("bonjour tout le monde");
-        System.out.print("bonjours");
+        System.out.print("bonjours  est ce que c'est  smahan");
     }
 }
